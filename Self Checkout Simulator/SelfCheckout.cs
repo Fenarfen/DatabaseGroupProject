453a725c9d9fdeaaecefbe512c5b6bb7037fd16e
Lewis Taylor <lewis.graham.taylor@gmail.com>
2021-12-06 19:41:06 +0000

    Added cases to SelfCheckout GetPromptForUser, not added logic to them

--- Self Checkout Simulator/SelfCheckout.cs
+++ Self Checkout Simulator/SelfCheckout.cs
@@ -7,13 +7,13 @@ namespace Self_Checkout_Simulator
     class SelfCheckout
     {
         // Attributes
-        
         private Product currentProduct;
 
         // Constructor
         public SelfCheckout(BaggingAreaScale baggingArea, ScannedProducts scannedProducts)
         {
             // TO DO
+
         }
 
         // Operations
@@ -21,11 +21,13 @@ namespace Self_Checkout_Simulator
         public void BarcodeWasScanned(int barcode)
         {
             // TO DO
+
         }
 
         public void BaggingAreaWeightChanged()
         {
             // TO DO
+
         }
 
         public void UserPaid()
@@ -38,6 +40,9 @@ namespace Self_Checkout_Simulator
             // TO DO: Use the information we have to produce the correct message
             //       e.g. "Scan an item.", "Place item on scale.", etc.
 
+            return "Scan an item";
+            return "Place item on scale";
+            return "ERROR: INCORRECT WEIGHT DETECTED";
             return "ERROR: UNKNOWN STATE";
         }
 
