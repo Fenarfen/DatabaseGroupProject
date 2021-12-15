547948a4d0aa22759ae681aa7110a26b4c0e1bf8
Smelli2307 <78549172+Smelli2307@users.noreply.github.com>
2021-12-15 09:58:06 +0000

    Add files via upload

---
+++ ClubcardDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Self_Checkout_Simulator
+{
+    public class QRCode
+    {
+
+        private static List<QRcode> QRcodes = new List<QRcode>
+            {
+
+            new QR(1025497, "Wes", "Atkinson", 100),
+            new QR(1232467, "Jamie", "Hufford", 1050),
+            new QR(1239467, "Ethan", "Micheal", 4375),
+            new QR(2075545, "Kate", "McGinn", 1400),
+            
+        };
+   
+
+        public static QRcode SearchUsingBarcode(int barcode)
+        {
+            return QRcodes.Find(p => p.GetQRcode() == barcode);
+        }
+
+        public static int GetRandomQRcode()
+        {
+            // filter the products to get only packaged items
+            var qrcodes = QRcodes.ToList();
+
+            // return a random one
+            return qrcodes[new Random().Next(qrcodes.Count)].GetQRcode();
+        }
+    }
+}
